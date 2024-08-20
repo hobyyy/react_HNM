7db576c0d5510523fac6ad4e5d7795fabdf83215
EunseoKim <dmstj9408@gmail.com>
2024-08-20 21:42:44 +0900

    :bookmark: Router 여러페이지 셋팅완료(1강끝)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router';
+import ProductAll from './page/ProductAll';
+import ProductDetail from './page/ProductDetail';
+import Login from './page/Login';
 // 1. 전체 상품 페이지, 로그인, 상품 상세 페이지
 // 2. 전체 상품 페이지 : 전체 상품을 볼 수 있다.
 // 3. 로그인 버튼 : 로그인 페이지가 나온다.
@@ -11,6 +14,11 @@ import './App.css';
 function App() {
   return (
     <div >
+      <Routes>
+        <Route path="" element={<ProductAll/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/product/:id" element={<ProductDetail/>}/>
+      </Routes>
     </div>
   );
 }
