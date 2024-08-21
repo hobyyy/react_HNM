8971a1d973cd7ce81d2ac797b89258761014256e
EunseoKim <dmstj9408@gmail.com>
2024-08-22 00:26:43 +0900

    :construction: 5강 API부르기까지 완성

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,18 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
-  // const getProducts = async() => {
-  //   // let url = `http://localhost:3000/products`;
-  //   // let response = await fetch(url);
-  //   // let data = await response.json();
-  //   // console.log('data : ', data)
-  // }
-  // useEffect(() => {
-  //   // getProducts()
-  // },[]);
+  const [productList, setProductList] = useState([]);
+  const getProducts = async() => {
+    let url = `http://localhost:4000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log('data : ', data)
+    setProductList(data);
+  }
+  useEffect(() => {
+    getProducts()
+  },[]);
   return (
     <div>
-      
+      <ProductCard/>
     </div>
   )
 }
